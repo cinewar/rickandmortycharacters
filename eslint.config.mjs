ca471ad40b65f9a1a5cee43845e01a55441ab03e
cinewar <49623430+cinewar@users.noreply.github.com>
2026-01-10 00:14:56 +0300

    prettier eslint husky added for better code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      semi: ["error"],
+      quotes: ["error", "double"],
+      "@typescript-eslint/no-explicit-any": ["error"],
+    },
+  }),
   {
     ignores: [
       "node_modules/**",
@@ -18,6 +25,7 @@ const eslintConfig = [
       "out/**",
       "build/**",
       "next-env.d.ts",
+      ".lintstagedrc.js",
     ],
   },
 ];
